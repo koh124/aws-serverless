05df296fdcdff0a180a851506c4bc0c72d220360
KoheiMori <koheimori@Mths20832.local>
2023-01-07 14:41:56 +0900

    created get/post sample API Gateway endpoint

---
+++ handler.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports.hello = async (event, context, callback) => {
+  const body = JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+      content: 'Hello World!'
+    },
+    null,
+    2
+  );
+
+  const response = {
+    statusCode: 200,
+    body: body
+  }
+
+  return response;
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.save = async (event, context, callback) => {
+  const body = JSON.stringify(
+    {
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    },
+    null,
+    2
+  );
+
+  const response = {
+    statusCode: 200,
+    body: body
+  }
+
+  return response;
+}
